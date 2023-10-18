d721cd22438e1ba522d3555c590e8e004c94afd1
MikePio <michelepiopilla@gmail.com>
2023-10-18 13:22:22 +0200

    Set security routes in the auth file

--- src/main/java/org/java/app/db/auth/conf/AuthConf.java
+++ src/main/java/org/java/app/db/auth/conf/AuthConf.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 // * STEP 6.1 - AUTHENTICATION - Creare il file AuthConf in db.auth.conf e compilarlo con filterChain, userDetailsService, passwordEncoder, authenticationProvider
 @Configuration
@@ -16,12 +17,25 @@ public class AuthConf {
   // * l'unico da modificare è sono i requestMatchers in filterChain mentre userDetailsService, passwordEncoder, authenticationProvider sono simili per tutti
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    
-    // todo da configurare le rotte
-    http.authorizeHttpRequests()
-    .requestMatchers("/**").permitAll()
-    .and().formLogin()
-    .and().logout();
+  
+    // * STEP 6.8 - AUTHENTICATION - aggiunte le rotte di accesso nel file di configurazione AuthConf 
+			http.csrf().disable().authorizeHttpRequests()
+      // ! questa rotta è necessaria oppure le immagini, lo style, bootstrap, fontawesome ed altro potrebbero non funzionare 
+      .requestMatchers("/css/**", "/js/**", "/img/**", "/webjars/**", "/font-awesome/**").permitAll()
+          // * .permitAll() fa accedere tutti: USER, ADMIN o anche un utente che non ha fatto il login
+          .requestMatchers("/login").permitAll()
+          .requestMatchers("/credits").permitAll()
+          // * protegge le rotte e ti fa accedere solo se sei USER o ADMIN
+          .requestMatchers("/").hasAnyAuthority("USER", "ADMIN")
+          .requestMatchers("/pizzas").hasAnyAuthority("USER", "ADMIN")
+          // .requestMatchers("/pizzas/**").hasAnyAuthority("USER", "ADMIN")
+          // OPPURE (Soluzione + precisa)
+          .requestMatchers(new RegexRequestMatcher("/pizzas/[0-9]+", null)).hasAnyAuthority("USER", "ADMIN")
+          // * protegge le rotte e ti fa accedere solo se sei ADMIN
+          .requestMatchers("/ingredients").hasAuthority("ADMIN")
+          .requestMatchers("/ingredients/**").hasAuthority("ADMIN")
+          .and().formLogin().defaultSuccessUrl("/")
+          .and().logout();
 
     return http.build();
 	}
